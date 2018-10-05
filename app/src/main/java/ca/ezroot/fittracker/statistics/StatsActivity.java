124d52cc4d5b684c0021c6a8ad6dbb3b6dda9f6a
EZroot <utohaha@gmail.com>
2018-10-05 16:47:57 -0230

    scrollable stats screen added, needs front-end work and to be filled with data

--- app/src/main/java/ca/ezroot/fittracker/statistics/StatsActivity.java
+++ app/src/main/java/ca/ezroot/fittracker/statistics/StatsActivity.java
@@ -2,7 +2,9 @@ package ca.ezroot.fittracker.statistics;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
@@ -22,11 +24,18 @@ public class StatsActivity extends AppCompatActivity
 	//Form data
 	ScrollView scrollView;
 	LinearLayout linearLayout;
+	CardView cardView;
+	RelativeLayout relativeLayout;
 	
 	TextView textViewWorkoutName;
 	TextView textViewWorkoutReps;
 	TextView textViewWorkoutSets;
 	TextView textViewWorkoutDate;
+	
+	//Multiple
+	CardView cardView2;
+	RelativeLayout relativeLayout2;
+	TextView[] textViewData;
 	
 	public void onInit()
 	{
@@ -37,10 +46,40 @@ public class StatsActivity extends AppCompatActivity
 		scrollView = new ScrollView(getApplicationContext());
 		linearLayout = (LinearLayout)findViewById(R.id.linearLayout);
 		
+		//Single card
+		cardView = (CardView)findViewById(R.id.cardid);
+		relativeLayout = (RelativeLayout)findViewById(R.id.relativelayoutid);
+		
 		textViewWorkoutName = (TextView)findViewById(R.id.workoutname);
 		textViewWorkoutReps = (TextView)findViewById(R.id.workoutreps);
 		textViewWorkoutSets = (TextView)findViewById(R.id.workoutsets);
 		textViewWorkoutDate = (TextView)findViewById(R.id.workoutdate);
+		
+		//Multiple cards
+		cardView2 = new CardView(getApplicationContext());
+		cardView2.setLayoutParams(cardView.getLayoutParams());
+		relativeLayout2 = new RelativeLayout(getApplicationContext());
+		relativeLayout2.setLayoutParams(relativeLayout.getLayoutParams());
+		
+		textViewData = new TextView[4]; //divide by 4 or increment by 4
+		textViewData[0] = new TextView(getApplicationContext()); //name
+		textViewData[1] = new TextView(getApplicationContext()); //reps
+		textViewData[2] = new TextView(getApplicationContext()); //sets
+		textViewData[3] = new TextView(getApplicationContext()); //date
+		
+		//params
+		textViewData[0].setLayoutParams(textViewWorkoutName.getLayoutParams());
+		textViewData[1].setLayoutParams(textViewWorkoutReps.getLayoutParams());
+		textViewData[2].setLayoutParams(textViewWorkoutSets.getLayoutParams());
+		textViewData[3].setLayoutParams(textViewWorkoutDate.getLayoutParams());
+		
+		relativeLayout2.addView(textViewData[0]);
+		relativeLayout2.addView(textViewData[1]);
+		relativeLayout2.addView(textViewData[2]);
+		relativeLayout2.addView(textViewData[3]);
+		
+		cardView2.addView(relativeLayout2);
+		linearLayout.addView(cardView2);
 	}
 	
 	public void displayData()
@@ -62,12 +101,19 @@ public class StatsActivity extends AppCompatActivity
 			/* for each saved workout, create a new linearlayout + children and FILL it*/
 			//for(int i = 0; i < statsData.getItemCount(); i++)
 			//{
-			textViewWorkoutName.setText("Workout: "+statsData.getLiftingDatabase().get(0).getName());
-			textViewWorkoutReps.setText("Reps: "+Integer.toString(statsData.getLiftingDatabase().get(0).getReps()));
-			textViewWorkoutSets.setText("Sets:" +Integer.toString(statsData.getLiftingDatabase().get(0).getSets()));
-			textViewWorkoutDate.setText(statsData.getLiftingDatabase().get(0).getTodaysDate().toString());
+			textViewWorkoutName.setText("Workout: "+statsData.getLiftingDatabase().get(1).getName());
+			textViewWorkoutReps.setText("Reps: "+Integer.toString(statsData.getLiftingDatabase().get(1).getReps()));
+			textViewWorkoutSets.setText("Sets:" +Integer.toString(statsData.getLiftingDatabase().get(1).getSets()));
+			textViewWorkoutDate.setText(statsData.getLiftingDatabase().get(1).getTodaysDate().toString());
+			
+			textViewData[0].setText("Workout: "+statsData.getLiftingDatabase().get(0).getName());
+			textViewData[1].setText("Reps: "+Integer.toString(statsData.getLiftingDatabase().get(0).getReps()));
+			textViewData[2].setText("Sets:" +Integer.toString(statsData.getLiftingDatabase().get(0).getSets()));
+			textViewData[3].setText(statsData.getLiftingDatabase().get(0).getTodaysDate().toString());
+			
 			//}
-			f.delete();
+			//f.delete();
+			
 		}
 	}
 }
